be620184b91c698a8178e321d82e19d415a709bf
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-01 21:53:51 -0600

    heavily rewriting the JS part to make sure major part of HTML is generated by JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
-// $("header").attr("style", "display: none;");
 var i = 0;
 var j = 0;
-var secondsLeft = 5;
-var correctAnswersCounter = 0;
-var wrongAnswersCounter = 0;
+var secondsLeft = 60;
+AnswersCounter = 0;
 
 var quizQuestions = [
-    "Thi111s is the question in question!",
-    "This is the question in 2question!",
-    "This is the ques5tion in q2uestion!",
-    "This is the ques5tion in q2uestion!",
+    "Commonly used data types DO NOT include:",
+    "String values must be enclosed within ... when being assigned to variables.",
+    "Arrays in JavaScript can be used to store ...",
+    "A very useful tool used during development and debugging for printing content to the debugger is:",
+    "The condition in an if/else statement is enclosed within ..."
 ]
 
 var quizAnswers = [
-    ["1", "22", "3", "4"],
-    ["3", "2", "1", "4"],
-    ["1", "4", "5", "2"],
-    ["2", "66", "2", "11"],
+    ["Strings", "Booleans", "Alerts", "Numbers"],
+    ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+    ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+    ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
 ]
 
 var correctAnswers = [
-    "2",
-    "3",
-    "1",
-    "66",
+    "Alerts",
+    "Quotes",
+    "All of the Above",
+    "Console.log",
+    "Parentheses",
 ]
 
 // Displays both Question and Answers 
@@ -33,23 +34,17 @@ function displayEverything() {
         var questionToAdd = $("<h4>");
         questionToAdd.attr("class", "question");
         questionToAdd.text(quizQuestions[i]);
-        questionToAdd.text(quizQuestions[i]);
         $("main").prepend(questionToAdd);
     }
     // Displays Answers
     function displayAnswers() {
-        for (j = 0; j < quizAnswers.length; j++) {
-            var buttonToAdd = $("<button>");
-            buttonToAdd.attr("class", "answer-button");
-            // buttonToAdd.attr("id", "button-" + (j + 1));
-            buttonToAdd.text(quizAnswers[i][j]);
-            $(".buttons").append(buttonToAdd);
+        for (j = 0; j < quizAnswers[i].length; j++) {
+            $(".buttons").append($("<button>").attr("class", "answer-button").text(quizAnswers[i][j]));
         }
     }
 
     displayQuestion();
     displayAnswers();
-    i++;
 }
 
 function clearEverything() {
@@ -57,12 +52,55 @@ function clearEverything() {
     $(".buttons").empty();
 }
 
-$(".answer-button").click(function () {
-    var thatText = $(this).text();
-    if (thatText === correctAnswers[i]) {
-        correctAnswersCounter += 1;
+function endQuiz() {
+    clearEverything();
+    $(".buttons").remove();
+
+    // Displaying End Header
+    $("main").prepend($("<h4>").attr("class", "end").text("End of the Quiz!"));
+
+    $("main").append($("input").attr("class", "initials"));
+    // Displaying input field
+
+    $("hr").remove();
+    $("#confirmation").remove();
+}
+
+function rightOrWrong(bool) {
+    if ($("hr") && $("#confirmation")) {
+        $("hr").remove();
+        $("#confirmation").remove();
+    }
+
+    if (bool) {
+        $("main").append($("<hr />"));
+        $("main").append($("<h3>").attr("id", "confirmation").text("Right answer!"));
+    } else {
+        $("main").append($("<hr />"));
+        $("main").append($("<h3>").attr("id", "confirmation").text("Wrong answer!"));
+    }
+}
+
+
+// Displaying Everything first time
+displayEverything();
+
+$(".buttons").click(function (event) {
+    if (event.target.textContent === correctAnswers[i]) {
+        rightOrWrong(true);
+        AnswersCounter += 10;
+        i++;
+        clearEverything();
+        displayEverything();
+    } else {
+        rightOrWrong(false);
+        AnswersCounter -= 10;
+        i++;
+        clearEverything();
+        displayEverything();
     }
 });
+
 
 var timerInterval = setInterval(function () {
     secondsLeft--;
@@ -70,10 +108,7 @@ var timerInterval = setInterval(function () {
 
     if (!secondsLeft || i === quizQuestions.length) {
         clearInterval(timerInterval);
-        clearEverything();
+        endQuiz();
         // TODO ADD WHATEVER HAPPENS WHEN TIMER'S OVER
     }
 }, 1000);
-
-
-displayEverything();
