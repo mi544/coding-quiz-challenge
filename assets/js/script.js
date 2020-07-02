bc3b18cb847702fbb3126666ba721ab2c7ff1115
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-01 19:54:28 -0600

    trying to wire up the main function to wait for a click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // $("header").attr("style", "display: none;");
-
+var i = -1;
+var j = 0;
 var secondsLeft = 60;
+var correctAnswersCounter = 0;
+var wrongAnswersCounter = 0;
+
 
 var timerInterval = setInterval(function () {
     secondsLeft--;
@@ -14,16 +18,23 @@ var timerInterval = setInterval(function () {
 
 var quizQuestions = [
     "Thi111s is the question in question!",
-    "This is the question in 2question!"
+    "This is the question in 2question!",
     "This is the ques5tion in q2uestion!",
-    "This is the ques5tion in q2uestion!"
+    "This is the ques5tion in q2uestion!",
 ]
 
 var quizAnswers = [
-    ["amswers", "answers", "asnwers", "asnwers"],
-    ["amswers", "answers", "asnwers", "asnwers"],
-    ["amswers", "answers", "asnwers", "asnwers"],
-    ["amswers", "answers", "asnwers", "asnwers"]
+    ["1", "2", "3", "4"],
+    ["3", "2", "1", "4"],
+    ["1", "4", "5", "2"],
+    ["2", "66", "2", "11"],
+]
+
+var correctAnswers = [
+    "2",
+    "3",
+    "1",
+    "66",
 ]
 
 // Checking if code is correct and number of questions matches number of objects of answers
@@ -32,18 +43,53 @@ if (quizQuestions.length !== quizAnswers.length) {
 }
 
 
-for (var i = 0; i < quizQuestions.length; i++) {
+
+// Displays Question
+function displayQuestion() {
     $(".question").text(quizQuestions[i]);
-    $(".question").attr("style", "display: block;")
-    for (var j = 1; j < 5; j++) {
-        $("#button-" + j).text(quizAnswers[i][j]);
-
-        $("#button-" + j).attr("style", "display: block;");
-    }
-
+    $(".question").attr("style", "display: block;");
 }
 
+// Displays Answers
+function displayAnswers() {
+    for (j = 0; j < quizAnswers.length; j++) {
+        $("#button-" + (j + 1)).text(quizAnswers[i][j]);
+        $("#button-" + (j + 1)).attr("style", "display: block;");
+    }
+}
+
+// Displays both Question and Answers 
+function displayEverything() {
+    i++;
+    displayQuestion(i);
+    displayAnswers(i);
+}
+
+displayEverything();
+
+$(".answer-button").click(function () {
+    var thatText = $(this).text();
+    if (thatText === correctAnswers[i]) {
+        correctAnswersCounter += 1;
+    }
+});
+
+// $(".answer-button").click(function () {
+//     if ($(this).text === correctAnswers[i]) {
+//         correctAnswersCounter += 1;
+//         console.log("a" + correctAnswersCounter);
+//         console.log("b" + wrongAnswersCounter);
+//         console.log("c" + correctAnswers[i]);
+//         console.log("d" + i);
 
 
-// Do this AFTER all the values are correct there
-$("main").attr("style", "display: block;");
+//         // TODO LESS TIMER NOW
+//     } else {
+//         wrongAnswersCounter += 1;
+//         console.log("a" + correctAnswersCounter);
+//         console.log("b" + wrongAnswersCounter);
+//         console.log("c" + correctAnswers[i]);
+//         console.log("d" + i);
+//     }
+
+// });
