559c6e7190dd86b7fbb4f54a1ca8013ff1edbc16
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-02 22:37:02 -0600

    finalizing the data.js file

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,28 +1,35 @@
-quizQuestions = {
-    que: [
-        "Commonly used data types DO NOT include:",
-        "String values must be enclosed within ... when being assigned to variables.",
-        "Arrays in JavaScript can be used to store ...",
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-        "The condition in an if/else statement is enclosed within ..."
-    ],
-    ans: [
-        ["Strings", "Booleans", "Alerts", "Numbers"],
-        ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
-        ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
-        ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
-        ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
-    ],
-    corq: [
-        "Alerts",
-        "Quotes",
-        "All of the Above",
-        "Console.log",
-        "Parentheses",
-    ]
-}
+var arr = {
+    quizQuestions: {
+        que: [
+            "Commonly used data types DO NOT include:",
+            "String values must be enclosed within ... when being assigned to variables.",
+            "Arrays in JavaScript can be used to store ...",
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+            "The condition in an if/else statement is enclosed within ...",
+        ],
+        ans: [
+            ["Strings", "Booleans", "Alerts", "Numbers"],
+            ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+            ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+            ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+            ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        ],
+        corA: [
+            "Alerts",
+            "Quotes",
+            "All of the Above",
+            "Console.log",
+            "Parentheses",
+        ]
+    },
 
-pageText = {
-    welcomeH: "Coding Quiz Challenge",
-    welcomeP: "Try to answer these simple code-related questions within the time limit.\nIncorrect answers will lead to time being subtracted from the timer.\nReady?"
+    pageText: {
+        welcomeH: "Coding Quiz Challenge",
+        welcomeP: [
+            "Welcome to the famous CODING QUIZ CHALLENGE.",
+            "Try to answer these simple code-related questions within the time limit.",
+            "Incorrect answers will lead to time being subtracted from the timer.",
+            "Ready?",
+        ]
+    }
 }
